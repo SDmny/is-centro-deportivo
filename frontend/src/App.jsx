bda9cc85fa3ca462d03bbdc43307a6699c014054
sianya <sianyidgm@gmail.com>
2026-03-24 15:28:52 -0600

    new route

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,14 +3,18 @@ import { Routes, Route } from "react-router-dom";
 /* interfaces */
 import CourseDetails from "./interfaces/courses/CourseDetails.jsx";
 import UserDashboard from "./interfaces/user/UserDashboard.jsx";
+import AdminManageCourses from "./interfaces/admin/AdminManageCourses.jsx";
 
 function App() {
   return (
     <>
       <Routes>
-        /* aqui van rutas */
+        {/* aqui van rutas */}
         <Route path="/" element={<UserDashboard />} />
         <Route path="/curso-detalle" element={<CourseDetails />} />
+
+        {/* como nota: la ruta / probablemente debería ser diferenciada según la sesión */}
+        <Route path="/admin" element={<AdminManageCourses />} />
       </Routes>
     </>
   );
