dbe96b9d9c421a73ef5074258be60120ef21f317
sianya <sianyidgm@gmail.com>
2026-03-28 14:43:02 -0600

    crear admin usuarios

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,17 +4,21 @@ import { Routes, Route } from "react-router-dom";
 import CourseDetails from "./interfaces/courses/CourseDetails.jsx";
 import UserDashboard from "./interfaces/user/UserDashboard.jsx";
 import AdminManageCourses from "./interfaces/admin/AdminManageCourses.jsx";
+import AdminManageUsers from "./interfaces/admin/AdminManageUsers.jsx";
 
 function App() {
   return (
     <>
       <Routes>
         {/* aqui van rutas */}
+
+        {/* como nota: la ruta / probablemente debería ser diferenciada según la sesión */}
         <Route path="/" element={<UserDashboard />} />
         <Route path="/curso-detalle" element={<CourseDetails />} />
 
-        {/* como nota: la ruta / probablemente debería ser diferenciada según la sesión */}
-        <Route path="/admin" element={<AdminManageCourses />} />
+        {/* rutas de administrador */}
+        <Route path="/admin_cursos" element={<AdminManageCourses />} />
+        <Route path="/admin_usuarios" element={<AdminManageUsers />} />
       </Routes>
     </>
   );
